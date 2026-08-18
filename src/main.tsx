5a6ad12a1a21df440a487372586354337ac800f5
giulia-ciampa <giulia.ciampa7@gmail.com>
2026-08-18 20:20:32 +0200

    added sass

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client"
 import "./index.css"
 import App from "./App.tsx"
+import "./styles/custom.scss"
 
 createRoot(document.getElementById("root")!).render(<App />)
